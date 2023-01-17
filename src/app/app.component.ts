55f6bc8e1284de5ac9a6ad86f82b8437d01f91c7
Yasir_Hassan <yasir.hassan7722@gmail.com>
2023-01-17 17:48:10 -0600

    adding navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-app';
+
+  loadedFeature = 'recipes';
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
